e292153a7306eed4a04fdfab7f084f8a6dd4e24a
Aakanksha Birajdar <aakankshabirajdar22@gmail.com>
2025-09-13 09:12:21 +0530

    update  delete and get apis of user

--- src/app.js
+++ src/app.js
@@ -8,51 +8,76 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/signup", async(req, res) =>{
-     const user = new User(req.body);
+app.post("/signup", async (req, res) => {
+    const user = new User(req.body);
 
-     try{
-         await user.save();
+    try {
+        await user.save();
         res.send("User Added Succesfully");
-     }catch(err){
+    } catch (err) {
         res.status(400).send("something Went wrong didnt save user");
-     }
-    
+    }
+
 })
 
+//Get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.email;
+
+    try {
+        const user = await User.find({ email: userEmail });
+        if (user.length == 0) {
+            res.status(404).send("User Not Found");
+        } else {
+            res.send(user);
+        }
+    } catch (err) {
+        res.status(400).send("Something Went Wrong");
+    }
+});
+
+//Feed API - get all users from the database
+app.get("/feed", async (req, res) => {
+    try {
+        const user = await User.find();
+        res.send(user);
+    } catch (err) {
+        res.status(400).send("Something Went Wrong");
+    }
+});
+
+app.delete("/user", async(req,res)=>{
+     const userId = req.body.userId;
+  try {
+
+        const user = await User.findByIdAndDelete(userId);
+        res.send("Deleted User Successfully");
+    } catch (err) {
+        res.status(400).send("Something Went Wrong");
+    }
+   
+});
+
+app.patch("/user" , async(req, res)=>{
+     const userId = req.body.userId;
+     const data = req.body; 
+       try {
+
+       await User.findByIdAndUpdate({_id :userId}, data);
+        res.send("Updated User Successfully");
+
+    } catch (err) {
+        res.status(400).send("Something Went Wrong");
+    }
+});
+
 connectDB()
-.then(()=>{
-    console.log("Database Connection Established");
-    app.listen(3000, ()=>{
-    console.log("Server is successfully listening on port 3000");
-});
-}).catch(err => {
-    console.log("Database Connection Failed");
-});
+    .then(() => {
+        console.log("Database Connection Established");
+        app.listen(3000, () => {
+            console.log("Server is successfully listening on port 3000");
+        });
+    }).catch(err => {
+        console.log("Database Connection Failed");
+    });
 
- 
-// 
-
-// const { adminAuth} = require("./middleware/auth");
-
-// app.use("/admin", adminAuth);
-
-// app.get("/user",(req, res)=>{
-//     res.send({firstname:"Aakanksha", lastname:"Birajdar"});
-// });
-
-// app.post("/user", (req, res)=>{
-//      res.send("Data Successfully saved to the database");
-// });
-// app.use("/test", (req, res)=>{
-//     res.send("Hello from server!");
-// });
-
-// app.use("/hello", (req, res)=>{
-//     res.send("Hello Hello Hello!");
-// });
-
-
-// app.listen(3000, ()=>{
-//     console.log("Server is successfully listening on port 3000");
-// });
