bceb150d9a579ddc2e6e3a9cefdd48b7d95e1101
Aakanksha Birajdar <aakankshabirajdar22@gmail.com>
2025-09-27 17:54:45 +0530

    logout and profile edit apis with validation

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const profileRouter = express.Router();
+const { validateEditProfileData } = require("../utils/validation");
+
 
 
 profileRouter.get("/profile", userAuth, async (req, res) => {
@@ -12,5 +14,28 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
         res.status(400).send("Error : " + err.message);
     }
 });
+
+profileRouter.patch("/profile/edit", userAuth, async (req,res)=>{
+
+    try {
+        if(!validateEditProfileData(req)){
+            throw new Error("Invalid Edit Request"); 
+        }
+
+        const loggedInUser = req.user;
+
+        Object.keys(req.body).forEach((key)=> (loggedInUser[key] = req.body[key]));
+
+        await loggedInUser.save();
+
+        res.json( {
+            message : `${loggedInUser.firstName} User Profile Updated Successful !!"`,
+            data: loggedInUser
+        });
+
+    } catch (err) {
+        res.status(400).send("Error : " + err.message);
+    }
+}); 
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,19 +2,27 @@ const validator = require("validator");
 
 const validateSignUpData = (req) => {
 
-    const {firstName, lastName, email, password} = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
-    if(!firstName || !lastName ){
+    if (!firstName || !lastName) {
         throw new Error("Name is not valid");
-    }else if(firstName.length<4 || firstName.length>50){
+    } else if (firstName.length < 4 || firstName.length > 50) {
         throw new Error("First name must be 4 - 50 Character");
-    }else if(!validator.isEmail(email)){
+    } else if (!validator.isEmail(email)) {
         throw new Error("Email is not valid");
-    }else if(!validator.isStrongPassword(password)){
+    } else if (!validator.isStrongPassword(password)) {
         throw new Error("Please Enter Strong Password!");
     }
 }
+const validateEditProfileData = (req) => {
+    const allowedEditFields = ["firstName", "lastName", "email", "photoUrl", "gender", "age", "about", "skills"];
 
-module.exports ={
-    validateSignUpData,
+    const editAllowed = Object.keys(req.body).every(field => 
+        allowedEditFields.includes(field)); 
+
+    return editAllowed;
+};
+
+module.exports = {
+    validateSignUpData, validateEditProfileData
 }
